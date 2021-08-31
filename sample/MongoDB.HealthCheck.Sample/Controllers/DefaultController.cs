92cce386b7976213d6e2116243ebe1feb0dce20c
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-08-31 15:59:19 -0500

    seal all the things

--- sample/MongoDB.HealthCheck.Sample/Controllers/DefaultController.cs
+++ sample/MongoDB.HealthCheck.Sample/Controllers/DefaultController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MongoDB.HealthCheck.Sample.Controllers
 {
 	[Route("")]
-	public class DefaultController : Controller
+	public sealed class DefaultController : Controller
 	{
 		[HttpGet]
 		public IActionResult Get() => Redirect("healthz");
