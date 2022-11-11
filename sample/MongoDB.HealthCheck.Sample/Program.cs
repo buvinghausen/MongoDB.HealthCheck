8405930f6d10bd22b7ca377560f154b823b3fa71
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2022-11-11 09:52:02 -0600

    Add gRPC health checks to the sample

--- sample/MongoDB.HealthCheck.Sample/Program.cs
+++ sample/MongoDB.HealthCheck.Sample/Program.cs
@@ -3,9 +3,31 @@ using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using MongoDB.Driver.Linq;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using ProtoBuf.Grpc.Server;
 using SequentialGuid;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure OpenTelemetry
+_ = builder.Services
+	.AddOpenTelemetryTracing(trace => trace
+		.SetResourceBuilder(ResourceBuilder.CreateDefault()
+			.AddService(builder.Environment.ApplicationName, serviceInstanceId: SequentialGuidGenerator.Instance.NewGuid().ToString()))
+		.AddAspNetCoreInstrumentation(o =>
+		{
+			o.RecordException = true;
+			o.EnableGrpcAspNetCoreSupport = true;
+		})
+		.AddMongoDBInstrumentation()
+		.AddConsoleExporter())
+	.AddControllers();
+
+// Add GrpcCodeFirst
+builder.Services
+	.AddCodeFirstGrpcReflection()
+	.AddCodeFirstGrpc();
+
+// Configure MongoDB for OpenTelemetry instrumentation
 var url = new MongoUrl(builder.Configuration.GetConnectionString("Mongo"));
 var settings = MongoClientSettings.FromUrl(url);
 settings.LinqProvider = LinqProvider.V3;
@@ -15,22 +37,27 @@ settings.ClusterConfigurator = cb =>
 		CaptureCommandText = true,
 		ShouldStartActivity = evt => string.IsNullOrWhiteSpace(url.DatabaseName) || evt.DatabaseNamespace.DatabaseName == url.DatabaseName
 	}));
+var database = new MongoClient(settings).GetDatabase(url.DatabaseName ?? "admin");
+
+// Add both Http & Grpc health checks
+// Note the preferred constructor is to use an IMongoDatabase which has all your settings, instrumentation, and configuration applied
 _ = builder.Services
 	.AddHealthChecks()
-	.AddMongoHealthCheck(new MongoClient(settings).GetDatabase(url.DatabaseName ?? "admin"), "Mongo"); // Note the preferred constructor is to use an IMongoDatabase which has all your settings, instrumentation, and configuration applied
+	.AddMongoHealthCheck(database, "MongoHttp"); 
 _ = builder.Services
-	.AddOpenTelemetryTracing(trace => trace
-		.SetResourceBuilder(ResourceBuilder.CreateDefault()
-			.AddService(builder.Environment.ApplicationName, serviceInstanceId: SequentialGuidGenerator.Instance.NewGuid().ToString()))
-		.AddAspNetCoreInstrumentation()
-		.AddMongoDBInstrumentation()
-		.AddConsoleExporter())
-	.AddControllers();
+	.AddGrpcHealthChecks()
+	.AddMongoHealthCheck(database, "MongoGrpc");
 
+// Build the application and map the endpoints
 var app = builder.Build();
 _ = app.MapControllers();
 _ = app.MapHealthChecks("/healthz");
+// /grpc.health.v1.Health/Check
+_ = app.MapGrpcHealthChecksService();
+// /grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo
+_ = app.MapCodeFirstGrpcReflectionService();
 
+// Run the app
 await app
 	.RunAsync()
 	.ConfigureAwait(false);
